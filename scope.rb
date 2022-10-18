a7b48159fcc1c06b749effcc2350f8c94b63a6b8
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2022-10-18 09:10:39 +0300

    complete

---
+++ scope.rb
@@ -0,0 +1,15 @@
+name = "faith"
+
+def greeting(name)
+    puts "Hello, #{name}"        
+end
+
+greet = greeting("sophie")
+
+evil_monster = "Bowser"
+
+def princess_peaches_castle
+  puts "#{evil_monster} is trying to kidnap Princess Peach!"
+end
+
+s= princess_peaches_castle
